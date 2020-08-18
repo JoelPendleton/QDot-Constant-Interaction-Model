543476816f7ca93e97c21efaf0452c80f2909228
Joel Pendleton <JoelPendleton@users.noreply.github.com>
2020-08-18 19:45:25 +0100

    Implementing quantum tunneling equation approach.

--- Python Files/simulation.py
+++ Python Files/simulation.py
@@ -11,22 +11,66 @@ import sys
 
 def calculate_current(V_SD, V_G, mu_N):
     e = 1.6E-19
-    h = 6.626E-34
+    h = 4.1357E-15
     k_B = 8.6173E-5
-    T = 0.1
+    T = 0.01
     R_K = h / (e **2)
-    R_T =  1/( V_G ** 2)
+    R_T =  1/(  V_G ** 2)
+
+
+    gammaPlus = np.ones(V_SD.shape)
+    gammaNegative = np.ones(V_SD.shape)
 
     deltaE = mu_N/e -  V_SD
-    gammaPlus = - (deltaE / h) * (R_K / R_T) / (1 - np.exp(deltaE / (k_B * T)))
-    #plt.plot(V_G[:,200], deltaE[200,:])
-    #plt.show()
-    sys.exit("finished")
+    exponent = deltaE / (k_B * T)
+    mask = exponent > 100
 
-    deltaE = mu_N/e
-    gammaNegative =  (deltaE / h) * (R_K / R_T) / (1 - np.exp(- deltaE / (k_B * T)))
+
+    gammaPlus[mask] = -0.01
+    mask = exponent < -100
+    gammaPlus[mask] = - (deltaE[mask] / h) * (R_K / R_T[mask])
+
+    '''
+    condition1 = exponent > -100
+    condition2 = exponent < 100
+    mask = (condition1 & condition2).astype(int)
+    print(mask.shape)
+
+    gammaPlus[mask] = - (deltaE[mask] / h) * (R_K / R_T[mask]) / (1 - np.exp([mask]))'''
+
+    '''
+    if exponent > 100:
+        gammaPlus = -0.01
+    elif exponent < -100:
+        gammaPlus = - (deltaE / h) * (R_K / R_T)
+    else:
+        gammaPlus = - (deltaE / h) * (R_K / R_T) / (1 - np.exp(exponent))
+    '''
+    deltaE = mu_N / e
+    exponent = -deltaE / (k_B * T)
+
+    mask = exponent > 100
+    gammaNegative[mask] = -0.01
+    mask = exponent < -100
+    gammaNegative[mask] = (deltaE[mask] / h) * (R_K / R_T[mask])
+
+
+    '''
+    condition1 = exponent > -100
+    condition2 = exponent < 100
+    mask = (condition1 & condition2).astype(int)
+    gammaNegative[mask] = (deltaE[mask] / h) * (R_K / R_T[mask]) / (1 - np.exp(exponent[mask]))'''
+
+    '''   
+    if exponent > 100:
+        gammaNegative = -0.01
+    elif exponent < -100:
+        gammaNegative = (deltaE / h) * (R_K / R_T)
+    else:
+        gammaNegative =  (deltaE / h) * (R_K / R_T) / (1 - np.exp(exponent))
+    '''
+
     current = e * gammaPlus * gammaNegative / (gammaPlus + gammaNegative)
-
     return current
 
 def electricPotential(n, V_SD_grid, V_G_grid, E_C, N_0, e, C_S, C_G):
@@ -41,7 +85,7 @@ def electricPotential(n, V_SD_grid, V_G_grid, E_C, N_0, e, C_S, C_G):
 
     E_N = E_C*(((n)**2-(n-1)**2)/n*5+random()/9*n)/(10*n)  # arbitrary random formula used to increase diamond width as more electrons are added
 
-    return E_N, (n - N_0 - 1/2) * E_C - (E_C / e) * (C_S * V_SD_grid + C_G * V_G_grid) #+ E_N
+    return E_N, (n - N_0 - 1/2) * E_C - (E_C / e) * (C_S * V_SD_grid + C_G * V_G_grid) + E_N
 
 
 def currentChecker(mu_N, mu_S, V_SD):
@@ -81,8 +125,8 @@ def generate(number_of_examples):
     # Define a 1D array for the values for the voltages
     # Define a 1D array for the values for the voltages
     V_SD_max = 0.1
-    V_G_min = 0.03
-    V_G_max = 1.5
+    V_G_min = 0.3
+    V_G_max = 1
     V_SD = np.linspace(- V_SD_max, V_SD_max, 1000)
     V_G = np.linspace(V_G_min, V_G_max, 1000)
 
@@ -122,121 +166,33 @@ def generate(number_of_examples):
 
             plt.figure(figsize=(10,10), dpi = 150)
 
-            Estate_height_previous = 0  # stores previous various excited energy height above ground level
 
             for n in N:
-                Estate_height = uniform(0.1, 0.5) * E_C
-                Lstate_height = uniform(0.5, 0.8) * E_C
 
                 # potential energy of ground to ground transition GS(N-1) -> GS(N)
                 E_N, mu_N = electricPotential(n, V_SD_grid, V_G_grid, E_C, N_0, e, C_S, C_G)
                 # Indices where current can flow for  GS(N-1) -> GS(N) transitions
-                allowed_indices = current_ground = currentChecker(mu_N, mu_S, V_SD)
-                current_ground = current_ground * calculate_current(V_SD_grid, V_G_grid, mu_N)
+                current_ground = calculate_current(V_SD_grid, V_G_grid, mu_N)
+                #print(np.max(current_ground))
+                #print(np.min(current_ground))
+
                 delta_E_N = E_N - E_N_previous  # Not sure on exact definition yet
                 delta_V_G = e/C_G + delta_E_N * C /(e *C_G) # Width of current diamond
 
                 if n ==1:
                     V_G_start = (e/C_G) * (E_N / E_C + 1/2)  # start of first diamond / start of current diamond
-                    diamond_starts[0,n-1] = V_G_start
-                    # potential energy of ground to excited transition GS(N-1) -> ES(N)
-                    mu_N_transition1 = mu_N + Estate_height
-
-                    mu_N_transition1 = np.multiply(mu_N_transition1, allowed_indices)
-                    '''This does element-wise multiplication
-                            with allowed_indices. Ensures current only flows / transition occurs only if ground state is free'''
-
-                    current_transition1 = currentChecker(mu_N_transition1, mu_S, V_SD)  # additional check if current can flow
-                    random_current_transition1 = current_transition1 * uniform(0.5, 2)
-                    '''random_current_transition1 adds some randomness to the current value'''
-
-                    I_tot += random_current_transition1 * calculate_current(V_SD_grid, V_G_grid, mu_N) * 0.1
 
                 elif n != 1:
                     V_G_start += delta_V_G  # update so start of current diamond
-                    diamond_starts[0, n-1] = V_G_start
-                    # The transitions from this block are to/from excited states
-
-                    # potential energy of  ground to excited transition GS(N-1) -> ES(N)
-                    mu_N_transition1 = mu_N + Estate_height
-                    mu_N_transition1 = np.multiply(mu_N_transition1, allowed_indices)
-                    current_transition1 = currentChecker(mu_N_transition1 , mu_S, V_SD)  # additional check if current can flow
-                    random_current_transition1 = current_transition1 * uniform(0.2, 2)
-                    '''This does element-wise multiplication
-                     with allowed_indices. Ensures current only flows / transition occurs only if ground state is free'''
-
-                    # potential energy of excited to ground transition GS(N-1) -> LS(N)
-                    mu_N_transition2 = mu_N + Lstate_height
-                    mu_N_transition2 = np.multiply(mu_N_transition2, allowed_indices)
-                    current_transition2 = currentChecker(mu_N_transition2, mu_S, V_SD)  # additional check if current can flow
-                    random_current_transition2 = current_transition2 * uniform(0.2, 2)
-
-                    # potential energy of excited to ground transition ES(N-1) -> GS(N)
-                    mu_N_transition3 = mu_N - Estate_height_previous
-                    mu_N_transition3 = np.multiply(mu_N_transition3, allowed_indices)
-                    current_transition3 = currentChecker(mu_N_transition3, mu_S, V_SD)  # additional check if current can flow
-                    random_current_transition3 = current_transition3 * uniform(0.2, 2)
-
-                    # potential energy of excited to ground transition ES(N-1) -> ES(N)
-                    mu_N_transition4 = mu_N - Estate_height_previous + Estate_height
-                    mu_N_transition4 = np.multiply(mu_N_transition4, allowed_indices)
-                    current_transition4 = currentChecker(mu_N_transition4, mu_S, V_SD)  # additional check if current can flow
-                    random_current_transition4 = current_transition4 * uniform(0.2, 2)
-
-                    # potential energy of excited to ground transition ES(N-1) -> LS(N)
-                    mu_N_transition5 = mu_N - Estate_height_previous + Lstate_height
-                    mu_N_transition5 = np.multiply(mu_N_transition5, allowed_indices)
-                    current_transition5 = currentChecker(mu_N_transition5, mu_S, V_SD)  # additional check if current can flow
-                    random_current_transition5 = current_transition5 * uniform(0.2, 2)
-
-                    # potential energy of excited to ground transition LS(N-1) -> GS(N)
-                    mu_N_transition6 = mu_N - Lstate_height_previous
-                    mu_N_transition6 = np.multiply(mu_N_transition6, allowed_indices)
-                    current_transition6 = currentChecker(mu_N_transition6, mu_S, V_SD)  # additional check if current can flow
-                    random_current_transition6 = current_transition6 * uniform(0.2, 2)
-
-                    # potential energy of excited to ground transition LS(N-1) -> ES(N)
-                    mu_N_transition7 = mu_N - Lstate_height_previous + Estate_height
-                    mu_N_transition7 = np.multiply(mu_N_transition7, allowed_indices)
-                    current_transition7 = currentChecker(mu_N_transition7, mu_S, V_SD)  # additional check if current can flow
-                    random_current_transition7 = current_transition7 * uniform(0.2, 2)
-
-                    # potential energy of excited to ground transition LS(N-1) -> LS(N)
-                    mu_N_transition8 = mu_N - Lstate_height_previous + Lstate_height
-                    mu_N_transition8 = np.multiply(mu_N_transition8, allowed_indices)
-                    current_transition8 = currentChecker(mu_N_transition8, mu_S, V_SD)  # additional check if current can flow
-                    random_current_transition8 = current_transition8 * uniform(0.2, 2)
-
-                    I_tot += (random_current_transition1 + random_current_transition2 + random_current_transition3 + \
-                             random_current_transition4 + random_current_transition5 + random_current_transition6 + \
-                             random_current_transition7 + random_current_transition8) \
-                             * calculate_current(V_SD_grid, V_G_grid, mu_N) * 0.1
-
-                # If statement is used as only transition to ground state is allowed for N = 1 from ground state
+                diamond_starts[0, n-1] = V_G_start
 
                 I_tot += current_ground
                 I_ground += current_ground
 
-                # update 'previous' variables to previous values
-                E_N_previous = E_N
-                Estate_height_previous = Estate_height
-                Lstate_height_previous = Lstate_height
 
 
 
-            I_tot += 10e-9
 
-            thermaNoise = np.random.normal(loc=1, scale=1, size=V_SD_grid.shape)
-            k_B = 1.38064852 * 10 ** (-23)
-            g_0 = I_tot / V_SD_grid
-            T = 0.01
-            I_thermalNoise = np.sqrt(4 * k_B * T * np.abs(g_0)) * thermaNoise
-            I_tot += I_thermalNoise
-
-            chargeNoise = np.random.normal(loc=1, scale=1, size=V_SD_grid.shape)
-            delta_f = 1000 # bandwidth
-            I_chargeNoise = np.sqrt(2 * e * I_tot * delta_f) * chargeNoise
-            I_tot += I_chargeNoise
 
 
             I_max = np.max(I_tot)
@@ -251,60 +207,20 @@ def generate(number_of_examples):
             plt.xlabel("$V_{G}$ / V")
             plt.ylabel("$V_{SD}$ / V")
 
-            ##plt.title("Single Quantum Dot Coulomb Blockade Simulation")
-            #cbar = plt.colorbar(contour)
-            #cbar.set_label("$I$ / A")
+            #plt.title("Single Quantum Dot Coulomb Blockade Simulation")
+            cbar = plt.colorbar(contour)
+            cbar.set_label("$I$ / A")
 
-            plt.axis("off")
-            plt.gca().xaxis.set_major_locator(plt.NullLocator()) # trick found on stackex. when trying to get rid of padding
-            plt.gca().yaxis.set_major_locator(plt.NullLocator())
-            plt.ylim([-V_SD_max, V_SD_max])
+            #plt.axis("off")
+            #plt.gca().xaxis.set_major_locator(plt.NullLocator()) # trick found on stackex. when trying to get rid of padding
+            #plt.gca().yaxis.set_major_locator(plt.NullLocator())
 
-            plt.savefig("../Training Data/Training_Input/input_{0}.png".format(k), bbox_inches='tight', pad_inches=0.0)
+            plt.savefig("../Training Data/Training_Input/input_{0}.png".format(k))#, bbox_inches='tight', pad_inches=0.0)
             plt.close()
 
-            # Compute negative and positive slopes of diamonds for drawing edges
-            positive_slope = C_G / (C_G + C_D)
-            negative_slope = - C_G / C_S
 
-            plt.figure(figsize=(10,10), dpi = 150)
 
-            for i in range(len(N)-1):  # need -1 as block would attempt to access index N otherwise and it doesn't exist
-                # positive grad. top-left
-                x_final = (positive_slope * diamond_starts[0, i] - negative_slope * diamond_starts[0, i + 1]) / (positive_slope - negative_slope)  # analytical formula derived by equating equations of lines
-                x_values = [diamond_starts[0, i], x_final]
-                y_final = positive_slope * (x_final - diamond_starts[0, i])
-                y_values = [0, y_final]
-                plt.plot(x_values, y_values, '-k')
 
-                # negative grad. top-right
-                x_values = [x_final, diamond_starts[0, i + 1]]
-                y_values = [y_final, 0]
-                plt.plot(x_values, y_values, '-k')
-
-                # positive grad. bottom-right
-                x_final = (positive_slope * diamond_starts[0, i + 1] - negative_slope * diamond_starts[0, i]) / (positive_slope - negative_slope)
-                x_values = [diamond_starts[0, i + 1], x_final]
-                y_final = positive_slope * (x_final - diamond_starts[0, i + 1])
-                y_values = [0, y_final]
-                plt.plot(x_values, y_values, '-k')
-
-                # negative grad. bottom-left
-                x_values = [x_final, diamond_starts[0, i]]
-                y_values = [y_final, 0]
-                plt.plot(x_values, y_values, '-k')
-
-            plt.axis("off")
-            plt.gca().xaxis.set_major_locator(plt.NullLocator()) # trick found on stackex. when trying to get rid of padding
-            plt.gca().yaxis.set_major_locator(plt.NullLocator())
-            plt.ylim([-V_SD_max, V_SD_max])
-            plt.xlim([V_G_min, V_G_max])
-
-            #plt.xlabel("$V_{G}$ / V")
-            #plt.ylabel("$V_{SD}$ / V")
-
-            plt.savefig("../Training Data/Training_Output/output_{0}.png".format(k), bbox_inches='tight', pad_inches=0.0)
-            plt.close()
 
             bar.update(k-1) # update progress bar
     return True
